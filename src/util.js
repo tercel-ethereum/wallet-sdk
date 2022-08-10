60455ac6f0a4ece1a97c405a162a8b68ba4ca948
tercel <tercel.yi@gmail.com>
2022-08-10 14:59:28 +0800

    added some utils

--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
-import { BigNumber } from 'ethers'
+import ethers, { BigNumber } from 'ethers';
 import BN from "bignumber.js";
 
 export function numberToDecimals(n, decimals) {
@@ -7,4 +7,26 @@ export function numberToDecimals(n, decimals) {
 
 export function numberFromDecimals(n, decimals) {
     return BigNumber.from(n).div(BigNumber.from(10).pow(decimals)).toString();
+}
+
+export function stringToHex(str) {
+    return ethers.utils.hexlify(ethers.utils.toUtf8Bytes(str));
+}
+
+export function hexToString(str) {
+    return ethers.utils.toUtf8String(str);
+}
+
+// eventName, include "event"
+export function parseEvent(logs, eventName) {
+    let iface = new ethers.utils.Interface(eventName);
+    // logs.forEach((log) => {
+    //     console.log(iface.parseLog(log));
+    // });
+    return logs.map(log=>iface.parseLog(log));
+}
+
+// eventAbi exclude "event"
+export function encodeEventTopics(eventAbi) {
+    return ethers.utils.keccak256(stringToHex(eventAbi));
 }
--- test/util.js
+++ test/util.js
@@ -1,12 +1,4 @@
-import ethers from 'ethers'
-
-function stringToHex(str) {
-    return ethers.utils.hexlify(ethers.utils.toUtf8Bytes(str));
-}
-
-function hexToString(str) {
-    return ethers.utils.toUtf8String(str);
-}
+import {stringToHex, hexToString, parseEvent, encodeEventTopics} from '../src/util.js';
 
 let res;
 res = stringToHex("Seaport");
@@ -16,4 +8,44 @@ res = Number("Seaport".length).toString(16);
 console.log(res);
 
 res = hexToString("0x536561706f7274");
+console.log(res);
+
+res = encodeEventTopics("Transfer(address,address,uint256)");
+console.log(res);
+
+
+let logs = [
+    {
+      "transactionIndex": 0,
+      "blockNumber": 23,
+      "transactionHash": "0xc410f22ba03c11af3768900268756767f4ccc00bef51d2598b45f7f9435cfc95",
+      "address": "0x2ef97f60B99611a0741B5ffA485DE4E595bA9411",
+      "topics": [
+        "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+        "0x000000000000000000000000b12a2f6faab46ae34d339672da774b5b49c556e4",
+        "0x0000000000000000000000000aba74d628aee342c5217edfa0f22dc948d5e675",
+        "0x000000000000000000000000000000002c8e2fff3c4b99196b919037a154b755"
+      ],
+      "data": "0x",
+      "logIndex": 0,
+      "blockHash": "0xb3f0b20287d08223595c1cb99933e799e1b9512c07eea61e5d671d7accea68d4"
+    },
+    {
+      "transactionIndex": 0,
+      "blockNumber": 23,
+      "transactionHash": "0xc410f22ba03c11af3768900268756767f4ccc00bef51d2598b45f7f9435cfc95",
+      "address": "0x2ef97f60B99611a0741B5ffA485DE4E595bA9411",
+      "topics": [
+        "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+        "0x000000000000000000000000b12a2f6faab46ae34d339672da774b5b49c556e4",
+        "0x0000000000000000000000000aba74d628aee342c5217edfa0f22dc948d5e675",
+        "0x00000000000000000000000000000000dd2be9c98041ab647cb87047e8d8b494"
+      ],
+      "data": "0x",
+      "logIndex": 1,
+      "blockHash": "0xb3f0b20287d08223595c1cb99933e799e1b9512c07eea61e5d671d7accea68d4"
+    }
+  ];
+let abi = ["event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)"];
+res = parseEvent(logs, abi);
 console.log(res);
